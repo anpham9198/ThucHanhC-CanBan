b3fc07454a072b9ac939fe055cfbe2f1df98a1e8
anpham9198 <anpham9198@gmail.com>
2024-10-05 02:07:31 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello,World!");
+        Console.WriteLine("Pham Quoc An");
+        DateTime LocalDate = DateTime.Now;
+        Console.WriteLine("Thoi gian hien tai la: " + LocalDate);
+
+        Console.ReadKey();
+    }
+}
